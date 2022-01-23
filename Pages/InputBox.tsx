532df40ff7ba6215baf9ac3419edd6e1c0070164
mehulkchaudhari <chaudharimehul2001@gmail.com>
2022-01-23 22:46:25 +0530

    added color.

--- Pages/InputBox.tsx
+++ Pages/InputBox.tsx
@@ -4,11 +4,26 @@ import { View, Text, TextInput } from "react-native";
 const InputBox = () => {
   const [text, setText] = useState("");
   const [remaining, setRemaining] = useState(280);
+  const [colour, setColour] = useState("");
 
   const onChangeHandler = (newText: string) => {
+    var remainingChar = 280 - newText.length;
+
     setText(newText);
-    setRemaining(280 - text.length);
+    setRemaining(remainingChar);
+    setColor(remaining);
   };
+
+  const setColor = (value: number) => {
+    if (value <= 10) {
+      setColour("red");
+    } else if (value < 30) {
+      setColour("gray");
+    } else {
+      setColour("black");
+    }
+  };
+
   return (
     <View>
       {/* <InputBox />
@@ -19,11 +34,17 @@ const InputBox = () => {
         placeholder="Type here to translate!"
         onChangeText={(newText) => onChangeHandler(newText)}
         defaultValue={text}
+        numberOfLines={10}
       />
 
-      <Text style={{ fontSize: 20 }}>{remaining} words remaining</Text>
+      <Text style={{ fontSize: 20, textDecorationColor: colour }}>
+        {remaining} words remaining
+      </Text>
     </View>
   );
 };
 
 export default InputBox;
+
+
+//TODO: fix this. Its temp. 
